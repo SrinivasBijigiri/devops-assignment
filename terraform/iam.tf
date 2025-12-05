5f2878816e9401704ef9759a03fb6976e32bf191
SrinivasBijigiri <151367971+SrinivasBijigiri@users.noreply.github.com>
2025-12-05 14:28:07 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,3 @@
-# IAM role for EC2 to send logs and use SSM
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -15,7 +14,6 @@ resource "aws_iam_role" "ec2_role" {
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
-# Attach AWS managed policies for SSM and CloudWatch logging
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
@@ -30,3 +28,4 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
+
